74c19a15f280b835c46aed5cbf3481c2f8b4af47
DaxtorT <110640402+DaxtorT@users.noreply.github.com>
2022-09-30 10:15:25 +1300

    finished instructions

---
+++ 07_instructions.py
@@ -0,0 +1,31 @@
+# Puts a series of symbols at the start and end of text (for emphasis)
+def statement_generator(text, decoration):
+
+    # Make string with five characters
+    ends = decoration * 5
+
+    # Add decoration to start and end of statement
+    statement = "{}  {}  {}".format(ends, text, ends)
+
+    print()
+    print(statement)
+    print()
+
+    return ""
+
+
+# Displays instructions / information
+def instructions():
+
+    statement_generator("Instructions / Information", "=")
+    print()
+    print("Please choose a data type (image / text / integer)")
+    print()
+    print("This program assumes that images are being represented in 24 bit colour (ie: 24 bits per pixel). For text, we assume that asci encoding is being used (8 bits per character).")
+    print()
+    print("Complete as many calculations as necessary, pressing <enter> at the end of each calculation or any key to quit.")
+    print()
+    return ""
+
+# Main Routine goes here
+instructions()
